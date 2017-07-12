20e5173b41b0ad8001f205d0ac0776fc7d8d3f42
yahmedov <ahmedov.yuksel@gmail.com>
2017-07-12 20:31:22 +0300

    script to clone repository to ./test and set chmod
    set permissions for directories and files

---
+++ .circleci/deploy-script.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/yahmedov/wp-ci.git /home/qualitya/public_html/test/;
+find /home/qualitya/public_html/test/ -type d -exec chmod 711 {} \;
+find /home/qualitya/public_html/test/ -type f -exec chmod 644 {} \;
